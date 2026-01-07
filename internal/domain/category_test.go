d94bba1c6599952c18c8813dc1d6703d217a87ad
Danilo Kodavara <danilo.kodavara@hotmail.com>
2026-01-06 21:59:51 -0300

    feat: update category test

---
+++ internal/domain/category_test.go
@@ -0,0 +1,14 @@
+package domain
+
+import "testing"
+
+// ... existing code ...
+func TestNewCategory(t *testing.T) {
+	category := Category{
+		Id: "1",
+	}
+
+	if category.Id != "1" {
+		t.Errorf("expected category Id to be '1', got '%s'", category.Id)
+	}
+}
