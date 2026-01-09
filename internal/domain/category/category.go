4418fb6c04da500681791277dd1dbf91fb9a4fda
Danilo Kodavara <danilo.kodavara@hotmail.com>
2026-01-08 22:30:24 -0300

    feat: update

--- internal/domain/category/category.go
+++ internal/domain/category/category.go
@@ -1,5 +1,56 @@
 package category
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type Category struct {
-	Id string
+	id          string
+	name        string
+	description string
+	isActive    bool
+	createdAt   time.Time
+	updatedAt   time.Time
+	deletedAt   *time.Time
+}
+
+func NewCategory(name, description string, isActive bool) *Category {
+	return &Category{
+		id:          uuid.NewString(),
+		name:        name,
+		description: description,
+		isActive:    isActive,
+		createdAt:   time.Now(),
+		updatedAt:   time.Now(),
+	}
+}
+
+func (c *Category) GetId() string {
+	return c.id
+}
+
+func (c *Category) GetName() string {
+	return c.name
+}
+
+func (c *Category) GetDescription() string {
+	return c.description
+}
+
+func (c *Category) IsActive() bool {
+	return c.isActive
+}
+
+func (c *Category) GetCreatedAt() time.Time {
+	return c.createdAt
+}
+
+func (c *Category) GetUpdatedAt() time.Time {
+	return c.updatedAt
+}
+
+func (c *Category) GetDeletedAt() *time.Time {
+	return c.deletedAt
 }
--- internal/domain/category/category_test.go
+++ internal/domain/category/category_test.go
@@ -1,14 +1,29 @@
 package category
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 // ... existing code ...
 func TestNewCategory(t *testing.T) {
-	category := Category{
-		Id: "1",
-	}
 
-	if category.Id != "1" {
-		t.Errorf("expected category Id to be '1', got '%s'", category.Id)
-	}
+	t.Run(`Given Valid Params When Call New Category Then Instantiate a Category`, func(t *testing.T) {
+		expectedName := "Filmes"
+		expectedDescription := "A categoria mais assistida"
+		expectedIsActive := true
+
+		expectedCategory := NewCategory(expectedName, expectedDescription, expectedIsActive)
+
+		assert.NotNil(t, expectedCategory)
+		assert.NotNil(t, expectedCategory.GetId())
+		assert.Equal(t, expectedName, expectedCategory.GetName())
+		assert.NotNil(t, expectedDescription, expectedCategory.GetDescription())
+		assert.True(t, true, expectedCategory.IsActive())
+		assert.NotNil(t, expectedCategory.GetCreatedAt())
+		assert.NotNil(t, expectedCategory.GetUpdatedAt())
+		assert.Nil(t, expectedCategory.GetDeletedAt())
+
+	})
 }
